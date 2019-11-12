65f468c02ccad88aaf0c545479c729348b6f881e
xia0 <939529983@qq.com>
2019-11-12 21:09:05 +0800

    add usage info

--- main.cpp
+++ main.cpp
@@ -3,16 +3,36 @@
 #include "il2cpp-metadata.h"
 #include "XB1nLib/XB1nLib.h"
 
-int main() {
-    XILOG("====XIL2CppDumper====\n");
+void printUasge(){
+    printf( "\n\n"
+            "+--------------------------------------------------------------------------------------+\n"
+            "| XIL2CppDumper | a tool of C++ version IL2CppDumper power by xia0@2019                |\n"
+            "+--------------------------------------------------------------------------------------+\n"
+            "| Usage         | XIL2CppDumper unity_metadata_file_path il2cpp_so_or_macho_file_path  |\n"
+            "+--------------------------------------------------------------------------------------+\n"
+            "| Blog          | http://4ch12dy.site                                                  |\n"
+            "+--------------------------------------------------------------------------------------+\n"
+            "| Github        | https://github.com/4ch12dy                                           |\n"
+            "+--------------------------------------------------------------------------------------+\n"
+            "| Specail thanks to Perfare's https://github.com/Perfare/Il2CppDumper                  |\n"
+            "+--------------------------------------------------------------------------------------+\n"
+            "\n"
+    );
+}
 
+int main(int argc, char* argv[]) {
+
+    if (argc != 3){
+        printUasge();
+        return 1;
+    }
     // init
-    XIL2CppDumper* xdump = XIL2CppDumper::GetInstance();
-    const char* metadataFileFullPath = "/Users/zhangshun/xia0/game-sec/XIL2CppDumper/resource/global-metadata.dat";
-    const char* il2cppbinFileFullPath = "/Users/zhangshun/xia0/game-sec/XIL2CppDumper/resource/ProductName";
+    const char* metadataFileFullPath = argv[1];
+    const char* il2cppbinFileFullPath = argv[2];
 
+    XIL2CppDumper* xdump = XIL2CppDumper::GetInstance();
     xdump->initMetadata(metadataFileFullPath, il2cppbinFileFullPath);
     xdump->dump();
-
+    
     return 0;
 }
